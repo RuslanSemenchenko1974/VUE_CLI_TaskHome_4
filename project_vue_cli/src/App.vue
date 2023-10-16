2d08e8d6ae0e98a0a4f460ac3d5814e898ad9ff5
Ruslan <89604587433@mail.ru>
2023-10-16 14:17:54 +0300

    HomeTask4 VUE CLI

---
+++ project_vue_cli/src/App.vue
@@ -0,0 +1,35 @@
+<template> 
+  
+<headerSight/>
+<mainFirstPage/>
+<footerSight/>
+<hr>
+<headerSight/>
+<mainTwoPage/>
+<footerSight/>
+<hr>
+<headerSight/>
+<mainThirdPage/>
+<footerSight/>
+</template>
+
+<script>
+import headerSight from './components/headerSight.vue';
+import mainTwoPage from './components/mainTwoPage.vue';
+import mainFirstPage from './components/mainFirstPage.vue';
+import mainThirdPage from './components/mainThirdPage.vue';
+import footerSight from './components/footerSight.vue';
+export default {  
+  components: {
+    headerSight,
+    footerSight,
+    mainTwoPage,
+    mainFirstPage,
+    mainThirdPage,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
